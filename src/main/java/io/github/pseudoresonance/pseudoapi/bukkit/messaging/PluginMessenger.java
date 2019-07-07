be798c7d7f783a2295e0d3699ab388a413742b6e
PseudoResonance <kaio11602@gmail.com>
2019-07-06 22:45:45 -0700

    Corrected plugin messenger

--- src/main/java/io/github/pseudoresonance/pseudoapi/bukkit/messaging/PluginMessenger.java
+++ src/main/java/io/github/pseudoresonance/pseudoapi/bukkit/messaging/PluginMessenger.java
@@ -19,20 +19,20 @@ public class PluginMessenger implements PluginMessageListener {
 
 	private static final String channelName = "pseudoapi:channel";
 	
-	private static final ArrayList<PluginMessageListener> listeners = new ArrayList<PluginMessageListener>();
-	private static final HashMap<PseudoPlugin, ArrayList<PluginMessageListener>> pluginListeners = new HashMap<PseudoPlugin, ArrayList<PluginMessageListener>>();
+	private static final ArrayList<PluginMessengerListener> listeners = new ArrayList<PluginMessengerListener>();
+	private static final HashMap<PseudoPlugin, ArrayList<PluginMessengerListener>> pluginListeners = new HashMap<PseudoPlugin, ArrayList<PluginMessengerListener>>();
 
 	public static void enable() {
 		Bukkit.getMessenger().registerOutgoingPluginChannel(PseudoAPI.plugin, channelName);
 		Bukkit.getMessenger().registerIncomingPluginChannel(PseudoAPI.plugin, channelName, new PluginMessenger());
 	}
 	
-	public static void registerListener(PseudoPlugin plugin, PluginMessageListener listener) {
-		ArrayList<PluginMessageListener> listenerList = null;
+	public static void registerListener(PseudoPlugin plugin, PluginMessengerListener listener) {
+		ArrayList<PluginMessengerListener> listenerList = null;
 		if (pluginListeners.containsKey(plugin)) {
 			listenerList = pluginListeners.get(plugin);
 		} else {
-			listenerList = new ArrayList<PluginMessageListener>();
+			listenerList = new ArrayList<PluginMessengerListener>();
 		}
 		listenerList.add(listener);
 		pluginListeners.put(plugin, listenerList);
@@ -41,8 +41,8 @@ public class PluginMessenger implements PluginMessageListener {
 	
 	public static void unregisterPluginListeners(PseudoPlugin plugin) {
 		if (pluginListeners.containsKey(plugin)) {
-			ArrayList<PluginMessageListener> listenerList = pluginListeners.get(plugin);
-			for (PluginMessageListener l : listenerList) {
+			ArrayList<PluginMessengerListener> listenerList = pluginListeners.get(plugin);
+			for (PluginMessengerListener l : listenerList) {
 				listeners.remove(l);
 			}
 			pluginListeners.remove(plugin);
@@ -53,8 +53,8 @@ public class PluginMessenger implements PluginMessageListener {
 		if (channel.equals(channelName)) {
 			ByteArrayDataInput in = ByteStreams.newDataInput(message);
 			String subchannel = in.readUTF();
-			for (PluginMessageListener listener : listeners) {
-				listener.onPluginMessageReceived(subchannel, p, message);
+			for (PluginMessengerListener listener : listeners) {
+				listener.onMessageReceived(p, subchannel, message);
 			}
 		}
 	}
@@ -62,25 +62,27 @@ public class PluginMessenger implements PluginMessageListener {
 	public static void sendToBungee(Player p, String channel, Collection<Object> data) {
 		ByteArrayDataOutput out = ByteStreams.newDataOutput();
 		out.writeUTF(channel);
-		for (Object o : data) {
-			if (o instanceof Boolean)
-				out.writeBoolean((boolean) o);
-			else if (o instanceof Byte)
-				out.writeByte((byte) o);
-			else if (o instanceof Character)
-				out.writeChar((char) o);
-			else if (o instanceof Double)
-				out.writeDouble((double) o);
-			else if (o instanceof Float)
-				out.writeFloat((float) o);
-			else if (o instanceof Integer)
-				out.writeInt((int) o);
-			else if (o instanceof Long)
-				out.writeLong((long) o);
-			else if (o instanceof Short)
-				out.writeShort((short) o);
-			else if (o instanceof String)
-				out.writeUTF((String) o);
+		if (data != null) {
+			for (Object o : data) {
+				if (o instanceof Boolean)
+					out.writeBoolean((boolean) o);
+				else if (o instanceof Byte)
+					out.writeByte((byte) o);
+				else if (o instanceof Character)
+					out.writeChar((char) o);
+				else if (o instanceof Double)
+					out.writeDouble((double) o);
+				else if (o instanceof Float)
+					out.writeFloat((float) o);
+				else if (o instanceof Integer)
+					out.writeInt((int) o);
+				else if (o instanceof Long)
+					out.writeLong((long) o);
+				else if (o instanceof Short)
+					out.writeShort((short) o);
+				else if (o instanceof String)
+					out.writeUTF((String) o);
+			}
 		}
 		p.sendPluginMessage(PseudoAPI.plugin, channelName, out.toByteArray());
 	}
